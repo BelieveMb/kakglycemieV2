42bf02babb0af566cbac59a09e7ab1ed16bb5eb9
Dev Unbelievable <mbolokobelieve@gmail.com>
2023-11-22 17:12:47 +0100

    blade of index and header

--- kakglycemie-webapp/resources/views/AllUsers/index.blade.php
+++ kakglycemie-webapp/resources/views/AllUsers/index.blade.php
@@ -1,1 +1,12 @@
-<h1>PAge d'accueil</h1>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienvenu.e sur KaK</title>
+    
+</head>
+<body>
+    
+</body>
+</html>
