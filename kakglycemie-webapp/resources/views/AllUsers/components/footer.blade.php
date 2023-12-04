62334f7172294fcf4e50a3b1e45fa4326451e77b
Dev Unbelievable <mbolokobelieve@gmail.com>
2023-12-04 14:25:23 +0100

    end all for index

--- kakglycemie-webapp/resources/views/AllUsers/components/footer.blade.php
+++ kakglycemie-webapp/resources/views/AllUsers/components/footer.blade.php
@@ -1,28 +1,20 @@
-<button onclick="topFunction()" id="btnTotop" title="Go to top" 
-  class="fixed border-none bg-red-500 text-white text-2xl p-5 rounded-xl"><i class="bi bi-arrow-up"></i></button>
+<button onclick="topFunction()" id="btnTotop" title="Go to top" class="bg-red-500/70 text-gray-200 hover:bg-gray-700 font-semibold" ><i class="bi bi-arrow-up"></i></button>
 <style>
-  #btnToto {
+  #btnTotop {
   position: fixed; /* Fixed/sticky position */
   bottom: 20px; /* Place the button at the bottom of the page */
   right: 30px; /* Place the button 30px from the right */
   z-index: 99; /* Make sure it does not overlap */
   border: none; /* Remove borders */
   outline: none; /* Remove outline */
-  background-color: red; /* Set a background color */
-  color: white; /* Text color */
   cursor: pointer; /* Add a mouse pointer on hover */
   padding: 15px; /* Some padding */
   border-radius: 10px; /* Rounded corners */
   font-size: 18px; /* Increase font size */
 }
-#btnTotop{
-  display: none;
 
-}
 
-#btnTotop:hover {
-  background-color: #555; /* Add a dark-grey background on hover */
-}
+
 </style>   
 
 <script>
