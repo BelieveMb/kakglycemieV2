432e1d52718219442ed405c1a3b9ae7244fa5d46
Dev Unbelievable <mbolokobelieve@gmail.com>
2023-12-04 12:22:38 +0100

    end script of medecin list

--- kakglycemie-webapp/resources/views/AllUsers/components/doctors_list.php
+++ kakglycemie-webapp/resources/views/AllUsers/components/doctors_list.php
@@ -43,25 +43,23 @@
 
 
 <section class="bg-gray-700 px-8 py-10  lg:px-40 lg:py-20 ">
-  <div
-    class="relative flex-[1_auto] flex flex-col break-words min-w-0 bg-clip-border rounded-[.95rem] border border-dashed border-stone-200 bg-white ">
+  <div class="relative flex-[1_auto] flex flex-col break-words min-w-0 bg-clip-border rounded-[.95rem] border border-dashed border-stone-200 bg-white ">
     <div class="flex-auto block py-2 px-8">
-      <h3 class="mb-2 text-[2rem] font-semibold text-gray-700">Nos Médecins </h3>
-      <div id="default-carousel" class="relative w-full" data-carousel="slide">
-        <div class="relative overflow-hidden rounded-lg h-20 lg:h-64">
+      <h3 class="mb-2 text-[2rem] font-semibold text-gray-700" >Nos Médecins </h3>
+      <div id="default-carousel" class="relative w-full" data-carousel="slide"  >
+        <div class="relative overflow-hidden rounded-lg h-40 lg:h-64">
           <!-- Item 1 -->
-          <div class="hidden duration-1000 ease-in-out lg:flex-row" data-carousel-item id="choiceDoctor">
-            <div class="flex flex-col lg:flex-row">
+          <div class="hidden duration-1000 ease-in-out lg:flex-row" data-carousel-item  >
+            <div class="flex flex-col lg:flex-row" >
               <div class="flex flex-col mr-5 text-center    ">
                 <div class="inline-block mb-4 relative shrink-0 rounded-[.95rem]">
                   <img class="inline-block shrink-0 rounded-[.95rem] w-[150px] h-[150px] shadow-red-200 shadow-lg"
-                    src="../images/img_med.png" alt="avarat image">
+                      src="../images/img_med.png" alt="avarat image">
                 </div>
-                <div class="text-center">
-                  <a href="javascript:void(0)"
-                    class="text-dark font-semibold hover:text-primary text-[1.25rem] transition-colors duration-200 ease-in-out">1
-                    Reynolds</a>
-                  <span class="block font-medium text-red-300">Marketing Manager</span>
+                <div class="text-center" >
+                  <a  class="text-dark font-semibold hover:text-primary text-[1.25rem] transition-colors duration-200 ease-in-out" id="choiceDoctor">1 Reynolds</a> <br>
+                    <button id="choiceDoctor" class="px-4 py-2  text-sm font-semibold bg-red-500 text-gray-50 rounded-lg hover:text-gray-600 focus:text-gray-900 hover:bg-red-200  hover:shadow-outline  focus:bg-red-200 focus:outline-none" >Chosir</button>
+          
                 </div>
               </div>
 
@@ -184,6 +182,104 @@
   </div>
 </section>
 
+<!-- modal bloc  -->
+  <div class="bg-white rounded-lg p-6 w-96 max-w-full shadow-lg transform transition-all duration-300 modal"  id="myModal" x-show.transition.opacity="showModal">
+      <div class="modal-content" >
+          <div class="flex justify-between items-center border-b-2 border-gray-200 pb-4">
+              <h2 class="text-2xl font-semibold">Connectez-vous ou inscrivez-vous <i class="bi bi-check-fill"></i></h2>
+              <button  class="text-gray-500 hover:text-gray-700 focus:outline-none close">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x">
+                      <line x1="18" y1="6" x2="6" y2="18"></line>
+                      <line x1="6" y1="6" x2="18" y2="18"></line>
+                  </svg>
+              </button>
+          </div>
+          
+          <div class="mt-6 space-y-4">
+              <p class="text-lg text-gray-600">Pour bénéficier de tous les avantages et commencer à prendre le contrôle de votre santé, veuillez vous connecter ou vous inscrire dès maintenant !</p>
+              <div class="flex flex-row space-x-4 justify-center">
+                  <a  href="{{ route('register') }}"  class="flex items-center justify-center gap-2 bg-gray-700 text-gray-100 px-4 py-2 rounded-lg hover:bg-gray-800 transition duration-300"> Inscrivez-vous <i class="bi bi-person-plus-fill"></i> </a>
+                  <a  href="{{ route('login') }}"  class="flex items-center justify-center gap-2 bg-red-500 text-gray-100 px-4 py-2 rounded-lg hover:bg-red-700 transition duration-300"> Connectez-vous <i class="bi bi-box-arrow-in-left"></i> </a>
+              </div>
+          </div>
+
+          <div class="mt-6 text-sm text-gray-500">
+              <p>Prenez le contrôle de votre diabète dès maintenant en vous connectant ou en vous inscrivant sur KaK glycémie !</p>
+          </div>
+      </div>
+  </div>
+<!-- modal bloc  -->
+
+  
+  
+
+
+<style>
+  /* The Modal (background) */
+.modal {
+  display: none; /* Hidden by default */
+  position: fixed; /* Stay in place */
+  z-index: 999; /* Sit on top */
+  left: 0;
+  top: 0;
+  width: 100%; /* Full width */
+  height: 100%; /* Full height */
+  overflow: hidden; /* Enable scroll if needed */
+  background-color: rgb(0,0,0); /* Fallback color */
+  background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+}
+
+/* Modal Content/Box */
+.modal-content {
+  background-color: #fefefe;
+  margin: 10% 30%; /* 15% from the top and centered */
+  padding: 20px;
+  border: 1px solid #888;
+  width: 40%; /* Could be more or less, depending on screen size */
+}
+
+/* The Close Button */
+.close {
+  color: #aaa;
+  float: right;
+  font-size: 28px;
+  font-weight: bold;
+}
+
+.close:hover,
+.close:focus {
+  color: black;
+  text-decoration: none;
+  cursor: pointer;
+}
+</style>
+    <!-- <script src="https://cdn.jsdelivr.net/npm/alpinejs@2.8.2/dist/alpine.min.js" defer></script> -->
 <script>
-  const choiceDoctor = 
+  var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("choiceDoctor");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+  
 </script>
+
