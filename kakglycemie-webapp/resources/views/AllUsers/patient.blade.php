34f1aa5eef4c2a9fd3bb3b6728f6889214780c5a
Dev Unbelievable <mbolokobelieve@gmail.com>
2023-12-08 14:41:57 +0100

    end style of section avantage patient

--- kakglycemie-webapp/resources/views/AllUsers/patient.blade.php
+++ kakglycemie-webapp/resources/views/AllUsers/patient.blade.php
@@ -50,7 +50,7 @@
     <section class="bg-gray-200 px-8  lg:px-40 py-20">
       <div class=" flex flex-wrap justify-between">
         <div class="w-full md:w-1/2 ">
-          <div class="">
+          <div class="my-4">
 
             <span class="text-gray-500">Pour vous</span>
             <h3 class="text-4xl font-semibold text-gray-700">Suivre sa glycémie</h3>
@@ -62,7 +62,7 @@
               <li class="py-2">
                 <div class="flex items-center">
                   <div>
-                    <span class="text-2xl font-semibold inline-block py-1 px-2 uppercase rounded-full text-gray-50 bg-red-500 mr-3"><i class="bi bi-person"></i></span>
+                    <span class="text-2xl font-semibold inline-block py-1 px-2 uppercase rounded-full text-gray-50 bg-red-500 mr-3"><i class="bi bi-graph-up"></i></span>
                   </div>
                   <div>
                     <p class="text-gray-700 font-semibold">Un suivi permet de détecter rapidement les fluctuations du taux de sucre dans le sang, ce qui peut être un signe d'un problème de santé sous-jacent. </p>
@@ -72,33 +72,34 @@
               <li class="py-2">
                 <div class="flex items-center">
                   <div>
-                    <span class="text-2xl font-semibold inline-block py-1 px-2 uppercase rounded-full text-gray-50 bg-red-500 mr-3"><i class="bi bi-person"></i></span>
+                    <span class="text-2xl font-semibold inline-block py-1 px-2 uppercase rounded-full text-gray-50 bg-red-500 mr-3"><i class="bi bi-exclamation-triangle"></i></span>
                   </div>
                   <div>
-                    <p class="text-gray-700 font-semibold">un suivi régulier de la glycémie peut aider à prévenir les complications liées au diabète, telles que les problèmes cardiaques, les problèmes rénaux et les problèmes de vision. </p>
+                    <p class="text-gray-700 font-semibold">détecter rapidement les variations anormales de leurs niveaux de sucre dans le sang et réagir rapidement et prendre les mesures nécessaires pour éviter des complications potentielles. </p>
                   </div>
                 </div>
               </li>
               <li class="py-2">
                 <div class="flex items-center">
                   <div>
-                    <span class="text-xl font-semibold inline-block py-1 px-2 uppercase rounded-full text-gray-50 bg-red-500 mr-3"><i class="bi bi-person"></i></span>
+                    <span class="text-xl font-semibold inline-block py-1 px-2 uppercase rounded-full text-gray-50 bg-red-500 mr-3"><i class="bi bi-heart"></i></span>
                   </div>
                   <div>
-                    <h4 class="text-gray-700 font-semibold">Amazing page examples</h4>
+                    <p class="text-gray-700 font-semibold">L'utilisation de Kak Glycémie pour suivre sa glycémie permet aux patients de prendre le contrôle de leur santé et d'améliorer leur qualité de vie</p>
                   </div>
                 </div>
               </li>
              
             </ul>
 
-            <div class="mt-8">
+            <div class="my-7 lg:mt-8">
               <a href="#" class="px-4 py-2  text-sm font-semibold bg-red-500 text-gray-50 rounded-lg hover:text-gray-600 focus:text-gray-900 hover:bg-red-200  hover:shadow-outline
               focus:bg-red-200 focus:outline-none"> Commencez </a>
             </div>
           
           </div>
         </div>
+
         <div class="w-full md:w-4/12 ">
           <img alt="image votre avantage sur kak" class="max-w-full h-full rounded-lg shadow-lg" src="{{ asset('images/img_avantage.jpg') }}">
         </div>
@@ -108,8 +109,8 @@
     <!-- section avantage patient -->
     <section class="  px-8 py-10 lg:px-40 lg:py-4 ">
       <div id="services" class="section bg-white">
-        <div >
-            <h3 class="text-3xl py-4 font-semibold text-gray-800">Votre <span class="text-red-600">gratuits</span> </h3>
+        <div class="mb-4" >
+            <h3 class="text-3xl py-4 font-semibold text-red-500"> Prendre en charge son diabète  <span class="text-gray-800">avec Kak Glycémie</span> </h3>
         </div>
 
         <div class="flex flex-wrap flex-row text-center">
@@ -118,12 +119,38 @@
               <div class="py-8 px-4 mb-12 bg-gray-400 border-b border-gray-700 rounded-xl transform transition duration-300 ease-in-out hover:-translate-y-2 hover:bg-gray-200">
                   <div class="inline-block text-gray-800 mb-4 text-5xl">
                       <!-- icon -->
-                      <span class="bi bi-search"></span>
+                      <span class="bi bi-stopwatch"></span>
                   </div>
-                  <h3 class="text-lg leading-normal mb-2 font-semibold text-black"> Prévention de complication</h3>
-                  <p class="text-gray-100">This is a wider card with supporting text below as a natural content.</p>
+                  <h3 class="text-lg leading-normal mb-2 font-semibold text-black"> Suivi précis et en temps réel</h3>
+                  <p class="text-gray-50 hover:text-gray-500 ">Vous pouvez suivre votre glycémie en temps réel et recevoir des notifications en temps réel lorsque vos mesures sont hors de la plage cible. Les données sont stockées dans le cloud et accessibles à tout moment, ce qui vous permet de suivre votre progression à distance.</p>
               </div>
           </div>
+
+          <div class="flex-shrink px-4 max-w-full w-full sm:w-1/2 lg:w-1/3 lg:px-6 wow fadeInUp" data-wow-duration="1s" style="visibility: visible; animation-duration: 1s; animation-name: fadeInUp;">
+              <!-- service block -->
+              <div class="py-8 px-4 mb-12 bg-gray-400 border-b border-gray-700 rounded-xl transform transition duration-300 ease-in-out hover:-translate-y-2 hover:bg-gray-200">
+                  <div class="inline-block text-gray-800 mb-4 text-5xl">
+                      <!-- icon -->
+                      <span class="bi bi-comment-dots"></span>
+                  </div>
+                  <h3 class="text-lg leading-normal mb-2 font-semibold text-black"> Échangez des messages sécurisés avec votre médecin </h3>
+                  <p class="text-gray-50 hover:text-gray-500 "> Kak Glycémie facilite la communication entre les patients diabétiques et leur médecin. Vous pouvez échanger des messages sécurisés avec votre médecin, ce qui permet une prise en charge personnalisée et rapide. </p>
+              </div>
+          </div>
+
+          <div class="flex-shrink px-4 max-w-full w-full sm:w-1/2 lg:w-1/3 lg:px-6 wow fadeInUp" data-wow-duration="1s" style="visibility: visible; animation-duration: 1s; animation-name: fadeInUp;">
+              <!-- service block -->
+              <div class="py-8 px-4 mb-12 bg-gray-400 border-b border-gray-700 rounded-xl transform transition duration-300 ease-in-out hover:-translate-y-2 hover:bg-gray-200">
+                  <div class="inline-block text-gray-800 mb-4 text-5xl">
+                      <!-- icon -->
+                      <span class="bi bi-clipboard2-data"></span>
+                  </div>
+                  <h3 class="text-lg leading-normal mb-2 font-semibold text-black"> Optimisez votre gestion du diabète avec Kak Glycémie. </h3>
+                  <p class="text-gray-50 hover:text-gray-500 "> Kak Glycémie offre une gestion personnalisée du diabète en fournissant des outils avancés d'analyse et de suivi. Grâce à des graphiques détaillés, vous pouvez visualiser les tendances de votre glycémie sur une période donnée...</p>
+              </div>
+          </div>
+
+          
 
         </div>
       </div>
