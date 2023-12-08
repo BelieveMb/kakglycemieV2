3b74e9c7a24b394cddd9929a722a4a6d2bfe679b
Dev Unbelievable <mbolokobelieve@gmail.com>
2023-12-08 14:07:53 +0100

    end responsive of section banner

--- kakglycemie-webapp/resources/views/AllUsers/patient.blade.php
+++ kakglycemie-webapp/resources/views/AllUsers/patient.blade.php
@@ -11,21 +11,21 @@
 <body>
     @include('AllUsers/components/header')
 
-    <section class="flex items-end  px-8 py-10 lg:px-40 lg:py-4 h-[70vh] bg-no-repeat bg-cover bg-center lg:h-[90vh] "   style="background-image: url('{{ asset('images/patient_kak.jpg') }}');">
+    <section class="flex items-end  px-8 py-10 lg:px-40 lg:py-4 h-[80vh] bg-no-repeat bg-cover bg-center lg:h-[90vh] "   style="background-image: url('{{ asset('images/patient_kak.jpg') }}');">
      
-      <div class="w-[80%] lg:w-[60%] h-[40vh]  py-4 px-8 border-2 border-red-500 rounded-2xl shadow-4xl shadow-gray-800 bg-gray-50 opacity-80 mb-4">
+      <div class="w-[90%] h-auto lg:w-[60%]  py-4 px-8  border-2 border-red-500 rounded-2xl shadow-4xl shadow-gray-800 bg-gray-50 opacity-80 mb-4 lg:text-xl">
           <span>KaK Glycémie pour le diabètique</span>
           <h1 class="mb-4 text-2xl font-extrabold tracking-tight leading-none text-red-500 md:text-35xl lg:text-5xl "> Patients par KaK</h1>
           <p class="mb-8 text-lg font-normal text-gray-800 lg:text-xl ">Suivez votre glycémie en tout simplicité.</p>
-        <div class="flex flex-col gap-4 lg:flex-row lg:justify-start lg:space-x-8">
-            <a href="#" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-red-500 hover:bg-red-300 focus:ring-4 focus:ring-red-300 ">
+        <div class="flex  justify-center gap-4 text-xs flex-row lg:justify-start lg:space-x-8 lg:text-xl">
+            <a href="{{ route('register') }}" class="inline-flex justify-center items-center py-1 px-3 lg:py-3 lg:px-5 text-base font-medium text-center text-white rounded-lg bg-red-500 hover:bg-gray-700 hover:text-gray-200 focus:ring-4 focus:ring-red-300 ">
               S'inscrire
               <span class="ml-2 -mr-1 text-lg" >
                 <i class="bi bi-arrow-right"></i>
               </span>
               
             </a>
-            <a href="#" class="inline-flex justify-center hover:text-gray-50 items-center py-3 px-5 text-base font-medium text-center border-gray-700 text-red-500 rounded-lg border  hover:bg-gray-700 focus:ring-4 focus:ring-gray-400 ml-4 ">
+            <a href="{{ route('login') }}" class="inline-flex justify-center hover:text-gray-50 items-center py-1 px-3 lg:py-3 lg:px-5 text-base font-medium text-center border-gray-700 text-red-500 rounded-lg border  hover:bg-gray-700 focus:ring-4 focus:ring-gray-400 ml-4 ">
               Se connecter
             </a>
           </div>
@@ -40,9 +40,9 @@
         <h3>Nous sommes là pour vous aider à suivre votre glycémie et vos traitements de manière simple et efficace.</h3>
 
       </div>
-      <div class="flex flex-col gap-4 lg:flex-row lg:space-x-8 justify-center ">
-        <a href="#" class="px-4 py-2  text-sm font-semibold bg-gray-100 text-red-500 rounded-lg  hover:text-red-600 focus:text-gray-900 hover:bg-gray-200  hover:shadow-outline focus:bg-gray-200 focus:outline-none"> Connexion  </a>
-        <a href="#" class="px-4 py-2  text-sm font-semibold border-gray-100 border-4 text-gray-50 rounded-lg  hover:text-gray-600 focus:text-gray-900 hover:bg-gray-50  hover:shadow-outline focus:bg-gray-200 focus:outline-none"> Inscription </a>
+      <div class="flex  gap-4 flex-row lg:space-x-8 justify-center ">
+         <a href="#" class="px-4 py-2  text-sm font-semibold bg-red-300 border-red-300 border-4  text-gray-50 rounded-lg hover:bg-red-500   hover:text-gray-50 focus:text-gray-900  hover:shadow-outline focus:bg-red-200 focus:outline-none"> Connexion <i class="bi bi-box-arrow-in-left"></i>  </a>
+         <a href="#" class="px-4 py-2  text-sm font-semibold border-gray-100 border-4 text-gray-50 rounded-lg  hover:text-gray-600 focus:text-gray-900 hover:bg-gray-50  hover:shadow-outline focus:bg-gray-200 focus:outline-none"> Inscription <i class="bi bi-person-plus-fill"></i></a>
       </div>
     </section>
 
