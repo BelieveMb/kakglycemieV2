55a885280a2c7a96f5ac2747256e68abc0349cab
Dev Unbelievable <mbolokobelieve@gmail.com>
2023-12-10 16:42:35 +0100

    feature register is all okay

--- kakglycemie-webapp/resources/views/auth/register.blade.php
+++ kakglycemie-webapp/resources/views/auth/register.blade.php
@@ -14,8 +14,8 @@
     @import url('https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/5.3.45/css/materialdesignicons.min.css');</style>
     <main>
         <section>
-                /* <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.js" defer></script>
-                <script src="https://cdnjs.cloudflare.com/ajax/libs/zxcvbn/4.4.2/zxcvbn.js"></script> */
+                 <!-- <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.js" defer></script>
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/zxcvbn/4.4.2/zxcvbn.js"></script>  -->
 
 <div class="min-w-screen min-h-screen bg-gray-500 flex items-center justify-center px-5 py-5">
     <div class="bg-gray-100 text-gray-500 rounded-3xl shadow-xl w-full overflow-hidden" style="max-width:1000px">
@@ -128,39 +128,39 @@
                     
                     <div class="flex flex-col gap-2">
                         <div class="w-full px-3 mb-5">
-                            <label for="email" class="text-xs font-semibold px-1" :value="__('password')">password </label>
+                            <label for="email" class="text-xs font-semibold px-1" :value="__('Email')">Email </label>
                             <div class="flex">
-                                <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i class="mdi mdi-account-outline text-gray-400 text-lg"></i></div>
-                                <input type="password" class="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" placeholder="Votre nom" name="password" :value="old('password')" required autofocus autocomplete="password" >
+                                <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i class="bi bi-envelope text-gray-400 text-lg"></i></div>
+                                <input type="mail" class="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" placeholder="Votre email" name="email" :value="old('email')" required autofocus autocomplete="email" >
                             </div>
-                            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                            <x-input-error :messages="$errors->get('email')" class="mt-2" />
                         </div>
 
                         <div class="w-full px-3 mb-5">
                             <label for="telPat" class="text-xs font-semibold px-1">Téléphone </label>
                             <div class="flex">
                                 <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i class="mdi mdi-account-outline text-gray-400 text-lg"></i></div>
-                                <input type="tel" class="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" placeholder="Votre nom" name="telPat" :value="old('telPat')" required autofocus autocomplete="telPat" >
+                                <input type="tel" class="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" placeholder="Votre numéro de téléphone" name="telPat" :value="old('telPat')" required autofocus autocomplete="telPat" >
                             </div>
                             <x-input-error :messages="$errors->get('telPat')" class="mt-2" />
                         </div>
                     </div>
 
-                    <div class="flex flex-col lg:flex-row gap-3">
+                    <div class="flex flex-row gap-3">
                         <div class="w-1/2 px-3 mb-5">
-                            <label for="password" class="text-xs font-semibold px-1" :value="__('Password')">Email </label>
+                            <label for="password" class="text-xs font-semibold px-1" :value="__('Password')">Mot de passe </label>
                             <div class="flex">
                                 <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i class="mdi mdi-account-outline text-gray-400 text-lg"></i></div>
-                                <input type="mail" class="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" placeholder="Votre nom" name="email" :value="old('email')" required autofocus autocomplete="email" >
+                                <input type="text" class="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" placeholder="Votre mot de passe" name="password" :value="old('password')" required autofocus autocomplete="password" >
                             </div>
-                            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                            <x-input-error :messages="$errors->get('password')" class="mt-2" />
                         </div>
 
                         <div class="w-1/2 px-3 mb-5">
-                            <label for="password_confirmation" class="text-xs font-semibold px-1" :value="__('Confirm Password')">Téléphone </label>
+                            <label for="password_confirmation" class="text-xs font-semibold px-1" :value="__('Confirm Password')">Confirmer votre mot de passe </label>
                             <div class="flex">
                                 <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i class="mdi mdi-account-outline text-gray-400 text-lg"></i></div>
-                                <input type="tel" class="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" placeholder="Votre nom" name="password_confirmation" :value="old('password_confirmation')" required autofocus autocomplete="password_confirmation" >
+                                <input type="text" class="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" placeholder="Votre nom" name="password_confirmation" :value="old('password_confirmation')" required autofocus autocomplete="password_confirmation" >
                             </div>
                             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
                         </div>
@@ -170,7 +170,9 @@
                     
                     <div class="flex -mx-3">
                         <div class="w-full px-3 mb-5">
-                            <button class="block w-full max-w-xs mx-auto bg-indigo-500 hover:bg-indigo-700 focus:bg-indigo-700 text-white rounded-lg px-3 py-3 font-semibold text-upp">S'inscrire</button>
+                            <button class="block w-full max-w-xs mx-auto bg-indigo-500 hover:bg-indigo-700 focus:bg-indigo-700 text-white rounded-lg px-3 py-3 font-semibold text-upp" type="submit">S'inscrire</button>
+                <!-- {{ __('Register') }} -->
+
                         </div>
                     </div>
                 </div>
