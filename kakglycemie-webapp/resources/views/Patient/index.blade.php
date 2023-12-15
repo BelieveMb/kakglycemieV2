65a83cbfc4c580919d6095cb1dd2cd544c05eebb
Dev Unbelievable <mbolokobelieve@gmail.com>
2023-12-15 13:33:44 +0100

    cutting the dashboard to components

--- kakglycemie-webapp/resources/views/Patient/index.blade.php
+++ kakglycemie-webapp/resources/views/Patient/index.blade.php
@@ -16,9 +16,16 @@
       @include('Patient.components.sidebar')
       <div class="bg-gray-900 opacity-50 hidden fixed inset-0 z-10" id="sidebarBackdrop"></div>
       <div id="main-content" class="h-full w-full bg-gray-50 relative overflow-y-auto lg:ml-64">
-        <div class="my-32 text-2xl">
-            <h1>LE contenu</h1>
+        <div class="pt-6 px-4">
+            <div class="w-full grid grid-cols-1 xl:grid-cols-2 2xl:grid-cols-3 gap-4">
+                @include('Patient.graphe')
+                @include('Patient.formAdd')
+
+            </div>
         </div>
+
+
+       
           @include('Patient.components.footer')
          <p class="text-center text-sm text-gray-500 my-10">
             &copy; 2019-2021 <a href="#" class="hover:underline" target="_blank">Themesberg</a>. All rights reserved.
