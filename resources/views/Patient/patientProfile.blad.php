38e14dfd43df86bfae173b24528c072756d001d4
Dev Unbelievable <mbolokobelieve@gmail.com>
2023-12-19 17:58:38 +0100

    start a profil blade

---
+++ resources/views/Patient/patientProfile.blad.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienvenu.e sur KaK /</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="{{ asset('../css/AllUsers.css') }}">
+
+</head>
+<body> 
+    <main>
+    <div>
+     @include('Patient.components.header')
+   <div class="flex overflow-hidden bg-white pt-16">
+      @include('Patient.components.sidebar')
+      <div class="bg-gray-900 opacity-50 hidden fixed inset-0 z-10" id="sidebarBackdrop"></div>
+      <div id="main-content" class="h-full w-full bg-gray-50 relative overflow-y-auto lg:ml-64">
+        <div class="pt-6 px-4">
+            <div class="w-full grid grid-cols-1 xl:grid-cols-2 2xl:grid-cols-3 gap-4">
+
+            </div>
+            <!-- component -->
+                <div class="max-w-lg mx-auto my-10 bg-white rounded-lg shadow-md p-5">
+                    <img class="w-32 h-32 rounded-full mx-auto" src="https://picsum.photos/200" alt="Profile picture">
+                    <h2 class="text-center text-2xl font-semibold mt-3">John Doe</h2>
+                    <p class="text-center text-gray-600 mt-1">Software Engineer</p>
+                    <div class="flex justify-center mt-5">
+                    <a href="#" class="text-blue-500 hover:text-blue-700 mx-3">Twitter</a>
+                    <a href="#" class="text-blue-500 hover:text-blue-700 mx-3">LinkedIn</a>
+                    <a href="#" class="text-blue-500 hover:text-blue-700 mx-3">GitHub</a>
+                    </div>
+                    <div class="mt-5">
+                    <h3 class="text-xl font-semibold">Bio</h3>
+                    <p class="text-gray-600 mt-2">John is a software engineer with over 10 years of experience in developing web and mobile applications. He is skilled in JavaScript, React, and Node.js.</p>
+                    </div>
+                </div>
+        </div>
+
+
+       
+          @include('Patient.components.footer')
+         <p class="text-center text-sm text-gray-500 my-10">
+            &copy; 2019-2021 <a href="#" class="hover:underline" target="_blank">Themesberg</a>. All rights reserved.
+         </p>
+      </div>
+   </div>
+   <script async defer src="https://buttons.github.io/buttons.js"></script>
+   <script src="https://demo.themesberg.com/windster/app.bundle.js"></script>
+</div>
+    
+    </main>
+</body>
+</html>
