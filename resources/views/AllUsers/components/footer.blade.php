63e2cbcb98466fc2f700bf26cd4fbab76b298746
Dev Unbelievable <mbolokobelieve@gmail.com>
2025-09-19 04:54:19 +0100

    footer is done

--- resources/views/AllUsers/components/footer.blade.php
+++ resources/views/AllUsers/components/footer.blade.php
@@ -67,20 +67,20 @@
                 <div class="sm:col-span-2">
                   <h3 class="max-w-lg text-xl font-semibold tracking-tight text-gray-800 xl:text-2xl dark:text-white">Laissez nous un message.</h3>
 
-                  <div class="flex flex-col mx-auto mt-6 space-y-3 md:space-y-0 md:flex-row">
+                  {{-- <div class="flex flex-col mx-auto mt-6 space-y-3 md:space-y-0 md:flex-row">
                       <input id="email" type="text" class="px-4 py-2 text-gray-700 bg-white border rounded-md dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-opacity-40 focus:ring-blue-300" placeholder="Email Address" />
               
                       <button class="w-full px-6 py-2.5 text-sm font-medium tracking-wider text-white transition-colors duration-300 transform md:w-auto md:mx-4 focus:outline-none bg-gray-800 rounded-lg hover:bg-gray-700 focus:ring focus:ring-gray-300 focus:ring-opacity-80">
                           Envoyer
                       </button>
                       
-                  </div>
+                  </div> --}}
                   <ul class="text-gray-50 text-xl my-4 flex gap-4">
-                    <li><a href="tel:+243817723066"><i class="bi bi-telephone"></i></a></li>
-                    <li><a href="mailto:mbolokobelieve@gmail.com"><i class="bi bi-envelope"></i></a></li>
+                    <li><a href="tel:+33615175061"><i class="bi bi-telephone"></i></a></li>
+                    <li><a href="mailto:djokiam@gmail.com"><i class="bi bi-envelope"></i></a></li>
                     <li><a href=""><i class="bi bi-facebook"></i></a></li>
-                    <li><a href=""><i class="bi bi-instagram"></i></a></li>
-                    <li><a href=""><i class="bi bi-whatsapp"></i></a></li>
+                    {{-- <li><a href=""><i class="bi bi-instagram"></i></a></li> --}}
+                    <li><a href="wa.me/33615175061"><i class="bi bi-whatsapp"></i></a></li>
                   </ul>
               </div>
             </div>
